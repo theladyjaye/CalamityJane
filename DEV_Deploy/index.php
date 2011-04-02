5df5ff3f82c711db9da5c24bfbfac21417ad8c61
Adam Venturella <aventurella@gmail.com>
2011-04-02 15:52:07 -0700

    added additional scripts

---
+++ DEV_Deploy/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+   "http://www.w3.org/TR/html4/strict.dtd">
+
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>index.php</title>
+	<meta name="generator" content="TextMate http://macromates.com/">
+	<meta name="author" content="Adam Venturella">
+	<!-- intentionally placing at top to test top and bottom placement -->
+	<script src="/resources/js/modules/project.advertisement.js"></script>
+	<script src="/resources/js/modules/project.services.js"></script>
+	<!-- Date: 2011-04-02 -->
+</head>
+<body>
+	<h1>Testing</h1>
+	<div>
+		<ul>
+			<li>Lorem</li>
+			<li>Ipsum</li>
+			<li>Dolor</li>
+			<li>Sit</li>
+		</ul>
+	</div>
+	<div>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+	<script src="/resources/js/jquery/jquery.js"></script>
+	<!-- Date: 2011-04-02 -->
+	<script src="/resources/js/application/application.main.js"></script>
+</body>
+</html>
