2c935fabefcb875a73e31d2ed7f2818b5bdb488d
Phil Tobias <philtobias@gmail.com>
2011-04-02 20:26:25 -0700

    Converted located_script to class and added it into rakefile.  When creating build the .php file will get updated with the compiled js file. todo: need to update for multiple files and not just .php

--- tools/build/locate_script.rb
+++ tools/build/locate_script.rb
@@ -1,44 +1,91 @@
 require 'digest/sha1'
-root = "../../DEV_Deploy"
-compiled_out = "/resources/js/compiled.js"
-scripts = {}
 
-Dir.glob(root + "/*.php") do |file|
-  File.open(file, "r") do |infile|
-    newFile = ""
-    while (line = infile.gets)
-      candidate = line.strip()
-      
-      if candidate.match(/^<script[^>]+><\/script>$/)
-        src = line.match(/src="([^"]+)"/)[1]
-        slash = /^\// =~  src ? '' : '/'
+=begin
+
+Locate script class
+All paths are now absolute
+
+@todo support multiple files, only compiling one file
+ 
+@param root_path        path to deploy files
+@param compiler_path    path to location of compiler
+=end
+class Locate_script
+  def initialize(root_path, compiler_path)
+    @root = root_path
+    @compiler_path = compiler_path
+    @compiled_out = "/resources/js/%compiled_file_name%.min.js"
+    @scripts = {}
+  end
+
+  def compile
+    compiled_js_file_name = find_files()
+    
+    command = "java -jar #{@compiler_path}/compiler.jar "
+
+    @scripts.each do | key, value |
+      command = command + "--js #{value} "
+    end
+
+    command = command + "--js_output_file " + @root + compiled_js_file_name
+    
+    # run shell command    
+    `#{command}`
+  end
+
+  def find_files
+    compiled_js_file_name = ""
+    Dir.glob("#{@root}/*.php") do |file|
+      File.open(file, "r") do |infile|
+        newFile = ""
+        while (line = infile.gets)
+          candidate = line.strip()
+
+          if candidate.match(/^<script[^>]+><\/script>$/)
+            src = line.match(/src="([^"]+)"/)[1]
+            slash = /^\// =~  src ? '' : '/' # <-- what is going on here? removing initial slash?
+          
+            # remove starting slash if exists
+            # %r is regex
+            src = src.sub!(%r{^\/}, "")             
+            
+            # hash off the contents of the entire file not the filename
+            # better way to ensure no duplicates
+            key = Digest::SHA1.hexdigest(File.read("#{@root}/#{src}"))
         
-        # hash off the contents of the entire file not the filename
-        # better way to ensure no duplicates
-        key = Digest::SHA1.hexdigest(File.read(root + slash + src))
-        
-        if !scripts.has_key?(key)
-          scripts[key] = root + slash + src
+            if !@scripts.has_key?(key)
+              @scripts[key] = "#{@root}/#{src}" #@root + slash + src
+            end
+          else
+            newFile = newFile + line
+          end
         end
-      else
-        newFile = newFile + line
+    
+        # create compiled js file name for current file
+        compiled_js_file_name = compiled_js_file(file)
+        
+        parts = newFile.split("</body>")
+        parts[0] = parts[0] + "\t<script src=\"#{compiled_js_file_name}\"></script>\n"
+        newFile =  parts.join("</body>\n")
+        
+        # by this point newFile represents the contents of the target file that should be written      
+        # overwrite file with compiled script name
+        File.open(file, 'w') {|f| f.write(newFile)}
       end
     end
     
-    parts = newFile.split("</body>")
-    parts[0] = parts[0] + "\t<script src=\"#{compiled_out}\"></script>\n"
-    newFile =  parts.join("</body>\n")
-    # by this point newFile represents the contents of the target file that should be written
-    puts "\n\nnewFile Path: #{file}\n\n"
+    return compiled_js_file_name
+  end
   
+  def compiled_js_file(file)
+    basename_with_rand = File.basename(file, '.*') + "-" + rand_num
+    return @compiled_out.gsub("%compiled_file_name%", basename_with_rand)
+  end
+    
+  # just need a random number to prevent conflicts in compiled js files
+  # this may be too much
+  # http://www.themomorohoax.com/2009/09/22/securerandom-stop-writing-your-own-random-number-and-string-generators  
+  def rand_num
+    return Digest::SHA1.hexdigest(Time.now.to_s.split(//).sort_by{rand}.join)
   end
 end
-
-command = "java -jar compiler.jar "
-
-scripts.each do | key, value |
-  command = command + "--js #{value} "
-end
-
-command = command + "--js_output_file " + root + compiled_out
-puts "Compile Command: \n\n#{command}\n"
